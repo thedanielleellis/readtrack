655a66e85ac78f69fb8058dc0041719643c82046
thedanielleellis <thedanielleellis@gmail.com>
2020-10-02 15:46:35 -0500

    added authentication

--- readtrack/app/controllers/application_controller.rb
+++ readtrack/app/controllers/application_controller.rb
@@ -5,10 +5,17 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'lovetoread'
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end 
+  end 
 end
--- readtrack/app/controllers/user_controller.rb
+++ readtrack/app/controllers/user_controller.rb
@@ -5,8 +5,16 @@ class UserController < ApplicationController
     end 
 
     post '/users/signup' do
-        @user = User.create(username: params[:username], password: params[:password])
-        redirect "/users/#{@user.id}"
+        if params[:username]== " " && params[:password]== " "
+            redirect "/users/signup"
+        else 
+            @user = User.create(
+                username: params[:username], 
+                password: params[:password]
+                )
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}"
+        end 
     end
 
     #show page 
@@ -14,4 +22,30 @@ class UserController < ApplicationController
         @user = User.find(params[:id])
         erb :'/users/show'
     end
+
+    get '/users/login' do
+        #if the user is loggined redirect to their homepage 
+        if logged_in?
+            @user = User.find(session[:user_id])
+            redirect "/users/#{@user.id}"
+        #else, show them the log in form 
+        else 
+            erb :'/users/login'
+        end
+    end 
+
+    post '/users/login' do 
+        #want to find the user, if it exists
+        @user = User.find_by(username: params[:username])
+        #if user exist, authenticate password
+        if @user && @user.authenticate(params[:password])
+            session[:user_id]=@user.id 
+            redirect "/users/#{@user.id}"
+        else 
+            redirect "/users/login"
+        end 
+    end 
+
+
+
 end 
